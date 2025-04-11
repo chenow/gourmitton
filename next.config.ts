a8f7888d06376d0ab6a7f178f905c91b5b745c91
Antoine Chéneau <antoine.cheneau@outlook.com>
2025-04-11 13:56:31 +0200

    chore(security): fix lighthouse errors on headers

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,54 @@
 import type { NextConfig } from "next";
 
+const cspHeader = `default-src 'self' *.cours.quimerch.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'self';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
     remotePatterns: [{ hostname: "*" }],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "no-referrer",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "geolocation=(self), microphone=()",
+          },
+        ],
+      },
+    ];
   },
 };
 
